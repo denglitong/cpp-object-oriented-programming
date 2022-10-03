2a8c95ecd9baee9ca58d394d5d1c8f033cfc4e6e
Litong Deng <litong.deng@compass.com>
2022-10-03 16:17:22 +0800

    class friends
    Signed-off-by: Litong Deng <litong.deng@compass.com>

--- Complex.cpp
+++ Complex.cpp
@@ -5,3 +5,11 @@
 #include "Complex.h"
 
 double Complex::image() const { return im; }
+
+Complex& Complex::operator+=(const Complex& rhs) {
+  // 同一个class的各个objects互为friends（友元）
+  // 所以这里 rhs 可以直接取私有成员
+  this->re += rhs.re;
+  this->im += rhs.im;
+  return *this;
+}
--- Complex.h
+++ Complex.h
@@ -20,23 +20,32 @@ class Complex {
   // C++ 的引用（类似于 C 中的指针，但比指针更漂亮，底层也是指针）
   // 引用/指针大小是四个字节，常用在大数据变量的传递上；
   // const reference 是不可更改的
+  // return by reference
   Complex& operator+=(const Complex&);
 
   // inline methods 函数定义在类body里面
   // 但是如果函数很复杂，编译器也无法给它force inline，最后是由编译器决定的
   // 同名函数，重载 overloading
+  // return by value
   double real() const { return re; }
 
   // 成员函数分 2 种：一种是会改变成员变量的，另一种是不会改变成员变量的，
   //  对于不会改变成员变量的，声明时加上 const，防止外部取到之后改变数据，
   //  所有的数据改变都要限制在成员函数内部；
   //  防止在外部声明了 const Complex 变量的情况下还能改变成员变量；
+  // return by value
   double image() const;
 
  private:
   double re, im;
   // 参数传递：pass by reference (to const)
-  friend Complex& __doapl(Complex&, const Complex&);
+  // 返回值传递: return by reference (to const)
+  // C++ friend 友员可以自由取得对方的 private 成员 而无需通过成员函数
+  friend Complex& __doapl(Complex* lhs, const Complex& rhs) {
+    lhs->re += rhs.re;
+    lhs->im += rhs.im;
+    return *lhs;
+  }
 };
 
 #endif  // CPP_OBJECT_ORIENTED_PROGRAMMING_COMPLEX_H
